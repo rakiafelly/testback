2352e38971f27cc32e1840aa9f1c252728cdb4c2
rakiafelly <rakiafelly14@gmail.com>
2022-07-22 21:29:03 +0100

    passport

--- passport/passport.js
+++ passport/passport.js
@@ -1,17 +1,17 @@
-// const passport = require('passport');
-// const jwt = require('jsonwebtoken');
-// const BearerStrategy = require('passport-http-bearer').Strategy;
-// const User = require('../models/auth');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const BearerStrategy = require('passport-http-bearer').Strategy;
+const User = require('../models/auth');
 
-// passport.use(new BearerStrategy(
-//     (token, done)=> {
-//         // console.log(token);
-//       const decodedData = jwt.verify(token, process.env.JWT_SECRET);
-//     //   console.log(decodedData);
-//       User.findById(decodedData.userId, (err, user)=> {
-//         if (err) { return done(err); }
-//         if (!user) { return done(null, false); }
-//         return done(null, user, { scope: 'all' });
-//       });
-//     }
-//   ));
+passport.use(new BearerStrategy(
+    (token, done)=> {
+        // console.log(token);
+      const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+    //   console.log(decodedData);
+      User.findById(decodedData.userId, (err, user)=> {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        return done(null, user, { scope: 'all' });
+      });
+    }
+  ));
